0becbdf2c203525b7034425fea216f6a56d1978a
Chris Kramins <chris.kramins+github@gmail.com>
2018-01-18 14:38:17 -0500

    Init commit for unifi windows container

---
+++ unifi/docker-entrypoint.ps1
@@ -0,0 +1,3 @@
+
+cd app/bin
+./start.bat
---
+++ unifi/test.ps1
@@ -0,0 +1,1 @@
+docker run -p 8080:8080  -p 8081:8081 -p 8443:8443 -p 8843:8843 -p 8880:8880 -v D:/temp/unifi:c:/app/data --rm -it kramins/unifi:windowsservercore powershell
