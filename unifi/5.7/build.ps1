eb2c824c004dbcf94392ec54fff875e10fa1ef8a
Chris Kramins <chris.kramins+github@gmail.com>
2018-06-06 22:43:27 -0400

    Moved to https://github.com/Kramins/docker-unifi-windows

--- unifi/5.7/build.ps1
+++
@@ -1,9 +0,0 @@
-$UNIFI_VERSION = "5.7.23"
-
-$imageFullName = ("kramins/unifi:{0}-windowsservercore" -f $UNIFI_VERSION)
-Write-Host `Building $imageFullName`
-docker build --build-arg UNIFI_VERSION=$UNIFI_VERSION . -t $imageFullName
-Write-Host "Tagging image as latest"
-docker tag $imageFullName  kramins/unifi:latest
-
-# docker build --build-arg UNIFI_VERSION=$UNIFI_VERSION . -t kramins/unifi
--- unifi/5.7/docker-entrypoint.ps1
+++
@@ -1,3 +0,0 @@
-
-cd app/bin
-./start.bat
